9648b2de4744598eb2b896487db35849b0dbd4d3
sebastianspisak <sebastian.spisak03@gmail.com>
2021-06-16 22:48:45 +0200

    assignment finished

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -21,3 +21,13 @@
  * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
  * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class HttpUrlChecker{
+    public static Matcher getMatcher(String http){
+        Pattern pattern = Pattern.compile("^(http)s?://([a-z]|[0-9])*[.]([a-z]|[.]|[-])*$");
+
+        return pattern.matcher(http);
+    }
+}
